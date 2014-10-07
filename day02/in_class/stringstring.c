4bbb2f321451c618416c5f293be7dc3987f5d417
Ryan Shipp <rshipp@mines.edu>
2014-10-07 13:28:47 -0600

    Use a buffer and take input.

--- day02/in_class/stringstring.c
+++ day02/in_class/stringstring.c
@@ -5,11 +5,25 @@ int main( int argc, char* argv[] )
 {
     int i;
 
+#if 0
     char* breadcrumb;
     for( i=0; i<argc; i++ ) {
         char* theArg = argv[i];
         char* token;
 
+        while( (token = strtok_r( theArg, " \t", &breadcrumb )) ) {
+            printf( "The next token is '%s'\n", token );
+            theArg = NULL;
+        }
+    }
+#endif
+
+    const int BUFLEN = 1024;
+    char buffer[BUFLEN];
+
+    while( fgets( buffer, BUFLEN, stdin ) && !feof(stdin) ) {
+        char* theArg = buffer;
+        char* token;
         while( (token = strtok_r( theArg, " \t", &breadcrumb )) ) {
             printf( "The next token is '%s'\n", token );
             theArg = NULL;
