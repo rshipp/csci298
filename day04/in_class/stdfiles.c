9c2ef4dbd596dc9c37142bd0babb0823004ff6e1
Ryan Shipp <rshipp@mines.edu>
2014-10-16 12:55:10 -0600

    Print the fd.

--- day04/in_class/stdfiles.c
+++ day04/in_class/stdfiles.c
@@ -7,10 +7,12 @@ int main(int argc, char* argv[])
     {
         if (i % 2)
         {
+            printf(">> fd %d << ", fileno(stdout));
             puts(argv[i]);
         }
         else
         {
+            fprintf(stderr, ">> fd %d << ", fileno(stderr));
             fputs(argv[i], stderr);
             fputs("\n", stderr);
         }
