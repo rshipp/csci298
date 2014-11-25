280c01951e688acd84bbe012708c71ab31fd047c
Ryan Shipp <rshipp@mines.edu>
2014-11-25 14:04:59 -0700

    Correctly reads and writes files.

--- project/crr.c
+++ project/crr.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <strings.h>
+#include <string.h>
 
 #include "crr.h"
 
@@ -31,12 +31,29 @@ int main(int argc, char* argv[])
         return 1;
     }
 
+#ifdef CRR_WRITESCHED
+    close(schedfile);
+    FILE* wschedfile = fopen(argv[2], "w");
+    if (!wschedfile) {
+        fprintf(stderr, "Error opening file '%s' for reading\n", argv[2]);
+        return 1;
+    }
+    struct Reservation* wsched = malloc(sizeof(struct Reservation));
+    int schedsize = 1;
+    fwrite(&schedsize, sizeof(int), 1, wschedfile);
+    strncpy(wsched->room, "Roomname\n", sizeof(wsched->room));
+    strncpy(wsched->description, "Desc ript ion.\n", sizeof(wsched->description));
+    wsched->start = 12;
+    wsched->end = 52;
+    writereservation(wschedfile, wsched);
+#endif
+
+
     /* Read in schedule data, if it exists. */
     struct Reservation** sched = readsched(schedfile);
     if (!sched) {
         return 1;
     }
-
 
     return 0;
 }
@@ -88,31 +105,26 @@ struct Reservation* readreservation(FILE* fp) {
         return NULL;
     }
     if(!fgets(r->room, sizeof(r->room), fp)) {
-        fputs("Error reading data\n", stderr);
+        fputs("Error reading data1\n", stderr);
         return NULL;
     }
     if(!fgets(r->description, sizeof(r->description), fp)) {
-        fputs("Error reading data\n", stderr);
+        fputs("Error reading data2\n", stderr);
         return NULL;
     }
     if (fread(&r->start, sizeof(r->start), 1, fp) != 1) {
-        fputs("Error reading data\n", stderr);
+        fputs("Error reading data3\n", stderr);
         return NULL;
     }
     if (fread(&r->end, sizeof(r->end), 1, fp) != 1) {
-        fputs("Error reading data\n", stderr);
+        fputs("Error reading data4\n", stderr);
         return NULL;
     }
 
     return r;
 }
 
-struct Reservation* writereservation(FILE* fp) {
-    struct Reservation* r = malloc(sizeof(struct Reservation));
-    if (!r) {
-        fputs("Error allocating memory\n", stderr);
-        return NULL;
-    }
+struct Reservation* writereservation(FILE* fp, struct Reservation* r) {
     if(!fputs(r->room, fp)) {
         fputs("Error writing data\n", stderr);
         return NULL;
@@ -139,16 +151,19 @@ struct Reservation** readsched(FILE* fp) {
         fputs("Error allocating memory\n", stderr);
         return NULL;
     }
-    int n;
-    for (n=0; n<BUFSIZE; n++) {
-        sched[n] = malloc(sizeof(struct Reservation)*MAXROOMLEN);
-        if (!sched[n]) {
+    int schedsize;
+    if (fread(&schedsize, sizeof(int), 1, fp) != 1) {
+        fputs("Error reading data\n", stderr);
+        return NULL;
+    }
+    int i, n;
+    int r = 1;
+    for (i=0; i<=schedsize-1; i++) {
+        sched[i] = malloc(sizeof(struct Reservation)*MAXROOMLEN);
+        if (!sched[i]) {
             fputs("Error allocating memory\n", stderr);
             return NULL;
         }
-    }
-    int r = 1, i = 0;
-    for (i=0; ; i++) {
         sched[i] = readreservation(fp);
         if (!sched[i]) {
             return NULL;
@@ -168,6 +183,9 @@ struct Reservation** readsched(FILE* fp) {
                 }
             }
         }
+    }
+    for (n=0; n<i; n++) {
+        printf("%s%s%d\n%d\n", sched[n]->room, sched[n]->description, sched[n]->start, sched[n]->end);
     }
 
     return sched;
