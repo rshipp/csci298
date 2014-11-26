d35739e56caeb4ac7126ed3d23f1902f3686f04b
george <rpubaddr0@gmail.com>
2014-11-26 01:42:04 -0700

    Fixed includes.

--- project/crr.h
+++ project/crr.h
@@ -1,9 +1,6 @@
 #ifndef CRR_CRR
 #define CRR_CRR
 
-#include <time.h>
-#include <stdio.h>
-
 #define BUFSIZE 1024
 #define MAXROOMLEN 50
 
--- project/handlers.c
+++ project/handlers.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <time.h>
 
 #include "crr.h"
 #include "crrses.h"
