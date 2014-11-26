7216d22e50f005cc72b7020f75ca00e6db96a57f
george <rpubaddr0@gmail.com>
2014-11-25 21:04:20 -0700

    Add curses windows and prompt, create Makefile.

--- project/crr.c
+++ project/crr.c
@@ -2,11 +2,94 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <ncurses.h>
+#include <ctype.h>
+#include <unistd.h>
 
 #include "crr.h"
 
+#define CORNER '+'
+#define VERT1  '|'
+#define VERT2  ':'
+#define HORZ1  '-'
+#define HORZ2  '='
+
+#define DISPLAY_TITLE "| crr |"
+#define EDIT_TITLE "| prompt |"
+#define QUIT_TITLE "| Esc->Quit |"
+/**
+ * For some reason, ncurses' KEY_ENTER is defined as 0x157, which
+ * corresponds to SHIFT+ENTER on my machine. Using the linefeed character
+ * instead seems to work. KEY_BACKSPACE is wrong too, so I'm using DEL.
+ * KEY_ESC is not defined at all.
+ */
+#define KEY_LF 0x0a
+#define KEY_DEL 0x7f
+#define KEY_ESC 0x1b
+
+
 #define BUFSIZE 1024
 #define MAXROOMLEN 50
+
+/* Stolen from charcell-resize.c */
+void draw_borders(WINDOW * screen, char horiz, char vert, char corner)
+{
+	int x, y, i;
+
+	getmaxyx(screen, y, x);
+
+	// 4 corners
+	mvwaddch(screen, 0, 0, corner);
+	mvwaddch(screen, y - 1, 0, corner);
+	mvwaddch(screen, 0, x - 1, corner);
+	mvwaddch(screen, y - 1, x - 1, corner);
+
+	// sides
+	for(i = 1; i < (y - 1); i++) {
+		mvwaddch(screen, i, 0, vert);
+		mvwaddch(screen, i, x - 1, vert);
+	}
+
+	// top and bottom
+	for(i = 1; i < (x - 1); i++) {
+		mvwaddch(screen, 0, i, horiz);
+		mvwaddch(screen, y - 1, i, horiz);
+	}
+}
+
+int size_display( WINDOW* display, WINDOW* edit )
+{
+	int parent_x, parent_y;
+	int edit_size = 3;
+
+	// always do this at startup or resize, otherwise you have strange initial
+	// refresh semantics
+	wclear(stdscr);
+	refresh();
+
+	// set up initial windows
+	getmaxyx(stdscr, parent_y, parent_x);
+
+	wresize(display, parent_y - edit_size, parent_x);
+	mvwin(display, 0, 0);
+	wclear(display);
+	draw_borders(display, HORZ1, VERT1, CORNER);
+	mvwprintw(display, 0, 3, DISPLAY_TITLE);
+	mvwprintw(display, 0, parent_x-3-strlen(QUIT_TITLE), QUIT_TITLE);
+
+	wresize(edit, edit_size, parent_x);
+	mvwin(edit, parent_y-edit_size, 0);
+	wclear(edit);
+	draw_borders(edit, HORZ2, VERT2, CORNER);
+	mvwprintw(edit, 0, 3, EDIT_TITLE);
+
+	wrefresh(display);
+	wrefresh(edit);
+
+	return parent_y - 4 - edit_size;
+}
+
+/* Main */
 
 int main(int argc, char* argv[])
 {
@@ -60,6 +143,66 @@ int main(int argc, char* argv[])
     for (i=0; i<roomslen; i++) {
         printf("%s", rooms[i]);
     }
+
+    /* curses */
+	initscr();
+	noecho();
+	cbreak();
+	keypad(stdscr,TRUE);
+	curs_set(FALSE);
+
+	// set up initial windows
+	WINDOW* display = newwin(1, 1, 0, 0 );
+	WINDOW* edit = newwin(1,1, 0, 0 );
+	int dispheight = size_display( display, edit );
+
+	int d = 0;
+    int e = 0;
+	char buf[BUFSIZE];
+    char line[BUFSIZE];
+    int ch;
+	while((ch = getch()) != KEY_ESC) {
+		switch (ch) {
+			case KEY_RESIZE:
+				strncpy( buf, "KEY_RESIZE", BUFSIZE );
+				mvwprintw( display, d++ + 2, 2, buf );
+				d = d % dispheight;
+				dispheight = size_display( display, edit );
+				break;
+			default :
+				if ( isprint(ch) ) {
+                    line[e] = (char)ch;
+					snprintf( buf, BUFSIZE, "%c", ch );
+					mvwprintw( edit, 1, ++e + 1, buf );
+					wrefresh(edit);
+                } else if ( ch == KEY_LF || ch == KEY_ENTER ) {
+                    line[e] = '\0';
+                    e = 0;
+                    snprintf( buf, BUFSIZE, "%s", line );
+                    mvwprintw( display, d++ + 2, 2, buf );
+                    d = d % dispheight;
+					wrefresh(display);
+                    wclear(edit);
+	                draw_borders(edit, HORZ2, VERT2, CORNER);
+	                mvwprintw(edit, 0, 3, EDIT_TITLE);
+                    wrefresh(edit);
+                } else if ( ch == KEY_DEL || ch == KEY_BACKSPACE ) {
+					snprintf( buf, BUFSIZE, "%c", ' ' );
+					mvwprintw( edit, 1, e + 1, buf );
+                    line[--e] = '\0';
+					wrefresh(edit);
+				} /*else {
+					snprintf( buf, BUFSIZE, "Unprintable %04x", ch );
+					mvwprintw( display, d++ + 2, 2, buf );
+					d = d % dispheight;
+					wrefresh(display);
+				}*/
+				break;
+		}
+    }
+
+	// close curses lib, reset terminal
+	endwin();
 
     return 0;
 }
