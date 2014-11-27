55070fe3268b4b001540daa8df03dd74f2a3edd1
george <rpubaddr0@gmail.com>
2014-11-26 21:25:54 -0700

    Use spaces, not tabs; removed commented code block.

--- project/crr.c
+++ project/crr.c
@@ -35,24 +35,6 @@ int main(int argc, char* argv[])
         return 1;
     }
 
-#ifdef CRR_WRITESCHED
-    close(schedfile);
-    FILE* wschedfile = fopen(argv[2], "w");
-    if (!wschedfile) {
-        fprintf(stderr, "Error opening file '%s' for reading\n", argv[2]);
-        return 1;
-    }
-    struct Reservation* wsched = malloc(sizeof(struct Reservation));
-    int schedsize = 1;
-    fwrite(&schedsize, sizeof(int), 1, wschedfile);
-    strncpy(wsched->room, "Roomname\n", sizeof(wsched->room));
-    strncpy(wsched->description, "Desc ript ion.\n", sizeof(wsched->description));
-    wsched->start = 12;
-    wsched->end = 52;
-    writereservation(wschedfile, wsched);
-#endif
-
-
     /* Read in schedule data, if it exists. */
     FILE* schedfile = fopen(argv[2], "r");
     struct Reservation** sched;
@@ -67,20 +49,20 @@ int main(int argc, char* argv[])
     }
 
     /* curses */
-	initscr();
-	noecho();
-	cbreak();
-	keypad(stdscr,TRUE);
-	curs_set(FALSE);
+    initscr();
+    noecho();
+    cbreak();
+    keypad(stdscr,TRUE);
+    curs_set(FALSE);
 
-	// set up initial windows
-	WINDOW* display = newwin(1, 1, 0, 0 );
-	WINDOW* edit = newwin(1,1, 0, 0 );
-	int dispheight = size_display( display, edit );
+    // set up initial windows
+    WINDOW* display = newwin(1, 1, 0, 0 );
+    WINDOW* edit = newwin(1,1, 0, 0 );
+    int dispheight = size_display( display, edit );
 
-	int d = 0;
+    int d = 0;
     int e = 0;
-	char buf[BUFSIZE];
+    char buf[BUFSIZE];
     char line[BUFSIZE];
     int ch;
     void* (*inputhandler)(char**, int, struct Reservation***, int*, struct Reservation**, struct Reservation***, WINDOW*, int, char*);
@@ -95,14 +77,14 @@ int main(int argc, char* argv[])
     writeline(display, dispheight, &d, buf, "4) Search and edit/delete reservations");
     inputhandler = main_handler;
 
-	while((ch = getch())) {
-		switch (ch) {
-			case KEY_RESIZE:
-				strncpy( buf, "KEY_RESIZE", BUFSIZE );
-				mvwprintw( display, d++ + 2, 2, buf );
-				d = d % dispheight;
-				dispheight = size_display( display, edit );
-				break;
+    while((ch = getch())) {
+        switch (ch) {
+            case KEY_RESIZE:
+                strncpy( buf, "KEY_RESIZE", BUFSIZE );
+                mvwprintw( display, d++ + 2, 2, buf );
+                d = d % dispheight;
+                dispheight = size_display( display, edit );
+                break;
             case KEY_ESC:
                 if (!confirmquit) {
                     writeline(display, dispheight, &d, buf, "Press Esc again to save and quit.");
@@ -123,38 +105,38 @@ int main(int argc, char* argv[])
                     endwin();
                     return 1;
                 }
-			default :
+            default :
                 confirmquit = 0;
-				if ( isprint(ch) ) {
+                if ( isprint(ch) ) {
                     line[e] = (char)ch;
-					snprintf( buf, BUFSIZE, "%c", ch );
-					mvwprintw( edit, 1, ++e + 1, buf );
-					wrefresh(edit);
+                    snprintf( buf, BUFSIZE, "%c", ch );
+                    mvwprintw( edit, 1, ++e + 1, buf );
+                    wrefresh(edit);
                 } else if ( ch == KEY_LF || ch == KEY_ENTER ) {
                     line[e] = '\0';
                     e = 0;
                     wclear(edit);
-	                draw_borders(edit, HORZ2, VERT2, CORNER);
-	                mvwprintw(edit, 0, 3, EDIT_TITLE);
+                    draw_borders(edit, HORZ2, VERT2, CORNER);
+                    mvwprintw(edit, 0, 3, EDIT_TITLE);
                     wrefresh(edit);
                     // Handle the input.
                     inputhandler = inputhandler(rooms, roomslen, &sched, &schedlen, &partial, &list, display, dispheight, line);
                     if (inputhandler == NULL) {
-	                    endwin();
+                        endwin();
                         return 1;
                     }
                 } else if ( ch == KEY_DEL || ch == KEY_BACKSPACE ) {
-					snprintf( buf, BUFSIZE, "%c", ' ' );
-					mvwprintw( edit, 1, e + 1, buf );
+                    snprintf( buf, BUFSIZE, "%c", ' ' );
+                    mvwprintw( edit, 1, e + 1, buf );
                     line[--e] = '\0';
-					wrefresh(edit);
-				}
-				break;
-		}
+                    wrefresh(edit);
+                }
+                break;
+        }
     }
 
-	// close curses lib, reset terminal
-	endwin();
+    // close curses lib, reset terminal
+    endwin();
 
     return 0;
 }
